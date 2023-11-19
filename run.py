e4ec498fad082a65f8cc8e29512910280dd3705b
Alex Hoenig <delimey@pm.me>
2023-11-19 11:13:28 +0000

    calculate surplus data

--- run.py
+++ run.py
@@ -64,9 +64,9 @@ def update_sales_worksheet(data):
     print('Sales worksheet updated successfully. \n')
     
 
-def calculate_surplus_data(sales_data):
+def calculate_surplus_data(sales_row):
     """
-    Compare sales with stock and calculate teh surplus for each sandwich type
+    Compare sales with stock and calculate the surplus for each sandwich type
 
     Surplus is defined as stock - sales:
     positive surplus indicates waste (thrown away stock)
@@ -75,7 +75,12 @@ def calculate_surplus_data(sales_data):
     print('Calculating surplus data ... \n')
     stock = SHEET.worksheet('stock').get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+        
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    return surplus_data
 
 def main():
     """
@@ -84,7 +89,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 print('\nWelcome to LoveSandwiches Data Automation\n')
 main()
